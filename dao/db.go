4e21c0d469a3f6b627889bee490aeaaa8a4f3fb2
aircjm <aircjm@gmail.com>
2020-07-12 14:18:11 +0800

    数据库配置

--- dao/db.go
+++ dao/db.go
@@ -1,22 +1,32 @@
 package dao
 
 import (
+	"fmt"
+	"github.com/aircjm/cardBox/config"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/pkg/errors"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 )
 
 var DB *gorm.DB
 var err error
 
-func init() {
-	// 移除数据库支持
-	//InitModel()
+func Init() (*gorm.DB, error) {
+	Conf := config.Conf
+	db, err := openDB(*Conf)
+	if err != nil {
+		return nil, errors.Wrap(err, "open database")
+	}
+
+	return db, db.DB().Ping()
 }
 
-func InitModel() {
-	DB, err = gorm.Open("postgres", "")
+func init() {
+	DB, err = Init()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
@@ -35,6 +45,67 @@ func InitModel() {
 	log.Println("Init DB Complete")
 }
 
-func InitTables() {
-	//DB.AutoMigrate(dto.AnkiNoteInfo{})
+func openDB(opts config.Config) (*gorm.DB, error) {
+	dsn, err := parseDSN(opts)
+	if err != nil {
+		return nil, errors.Wrap(err, "parse DSN")
+	}
+
+	return gorm.Open(opts.DataBase.Type, dsn)
+}
+
+func parseDSN(opts config.Config) (dsn string, err error) {
+	// In case the database name contains "?" with some parameters
+
+	databaseConfig := opts.DataBase
+	concate := "?"
+	if strings.Contains(databaseConfig.Name, concate) {
+		concate = "&"
+	}
+
+	switch databaseConfig.Type {
+	case "mysql":
+		if databaseConfig.Host[0] == '/' { // Looks like a unix socket
+			dsn = fmt.Sprintf("%s:%s@unix(%s)/%s%scharset=utf8mb4&parseTime=true",
+				databaseConfig.User, databaseConfig.Password, databaseConfig.Host, databaseConfig.Name, concate)
+		} else {
+			dsn = fmt.Sprintf("%s:%s@tcp(%s)/%s%scharset=utf8mb4&parseTime=true",
+				databaseConfig.User, databaseConfig.Password, databaseConfig.Host, databaseConfig.Name, concate)
+		}
+
+	case "postgres":
+		host, port := parsePostgreSQLHostPort(databaseConfig.Host)
+		if host[0] == '/' { // looks like a unix socket
+			dsn = fmt.Sprintf("postgres://%s:%s@:%s/%s%ssslmode=%s&host=%s",
+				url.QueryEscape(databaseConfig.User), url.QueryEscape(databaseConfig.Password), port, databaseConfig.Name, concate, databaseConfig.SSLMode, host)
+		} else {
+			dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s%ssslmode=%s",
+				url.QueryEscape(databaseConfig.User), url.QueryEscape(databaseConfig.Password), host, port, databaseConfig.Name, concate, databaseConfig.SSLMode)
+		}
+
+	//case "mssql":
+	//	host, port := parseMSSQLHostPort(opts.Host)
+	//	dsn = fmt.Sprintf("server=%s; port=%s; database=%s; user id=%s; password=%s;",
+	//		host, port, opts.Name, opts.User, opts.Password)
+	//
+	//case "sqlite3":
+	//	dsn = "file:" + opts.Path + "?cache=shared&mode=rwc"
+
+	default:
+		return "", errors.Errorf("unrecognized dialect: %s", databaseConfig.Type)
+	}
+
+	return dsn, nil
+}
+
+func parsePostgreSQLHostPort(info string) (host, port string) {
+	host, port = "127.0.0.1", "5432"
+	if strings.Contains(info, ":") && !strings.HasSuffix(info, "]") {
+		idx := strings.LastIndex(info, ":")
+		host = info[:idx]
+		port = info[idx+1:]
+	} else if len(info) > 0 {
+		host = info
+	}
+	return host, port
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.14
 
 require (
 	github.com/adlio/trello v1.1.0
+	github.com/fatih/camelcase v1.0.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.10
+	github.com/koding/multiconfig v0.0.0-20171124222453-69c27309b2d7 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 	golang.org/x/sys v0.0.0-20190812073006-9eafafc0a87e // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
